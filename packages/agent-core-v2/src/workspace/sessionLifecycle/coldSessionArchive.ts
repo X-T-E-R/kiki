7723qqq/kimi-code

fix(agent-core-v2): let session archive proceed after a failed resume (#3139)
A failed resume is cached by SessionManager and rethrown from
whenResumeSettled, so archiving a session whose workspace is gone
failed with the stale resume error even though cold archive only
rewrites session metadata. Swallow the settle failure: still wait for
an in-flight resume before the live/cold classification, but fall
through to the cold metadata path after a failed one.

packages/agent-core-v2/src/workspace/sessionLifecycle/coldSessionArchive.ts
@@ -72,7 +72,7 @@ export async function setSessionArchived(
 ): Promise<ColdSessionArchiveOutcome> {
   const manager = accessor.get(ISessionManager);
   return manager.withLifecycleSerialization(sessionId, async (unguarded) => {
-    await manager.whenResumeSettled(sessionId);
+    await manager.whenResumeSettled(sessionId).catch(() => undefined);
     const live = getLiveSessionById(accessor, sessionId);
     if (live !== undefined) {
       if (archived) await unguarded.archive();

X-T-E-R/kiki

(cherry picked from commit 381142aff1d165f4bf67327035afec81ab4f656b)

packages/agent-core-v2/src/workspace/sessionLifecycle/coldSessionArchive.ts
@@ -77,7 +77,7 @@ export async function setSessionArchivedBatch(
     try {
       const manager = accessor.get(ISessionManager);
       return await manager.withLifecycleSerialization(id, async (unguarded) => {
-        await manager.whenResumeSettled(id);
+        await manager.whenResumeSettled(id).catch(() => undefined);
         const live = getLiveSessionById(accessor, id);
         if (live !== undefined) {
           if (archived) await unguarded.archive();